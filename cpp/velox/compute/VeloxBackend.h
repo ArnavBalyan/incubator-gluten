a25c170e711315a7fb30c2216160548226d3056a
nimesh.k <nimesh.k@uber.com>
2025-04-28 12:23:29 +0000

    add comment for teardown

--- cpp/velox/compute/VeloxBackend.h
+++ cpp/velox/compute/VeloxBackend.h
@@ -63,6 +63,9 @@ class VeloxBackend {
   void tearDown() {
     // First, shutdown the CryptoFactory
     facebook::velox::parquet::shutdownCryptoFactory();
+    // Destruct IOThreadPoolExecutor will join all threads.
+    // On threads exit, thread local variables can be constructed with referencing global variables.
+    // So, we need to destruct IOThreadPoolExecutor and stop the threads before global variables get destructed.    
     ioExecutor_.reset();
   }
 
